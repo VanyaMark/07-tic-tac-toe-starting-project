ef2f3774c5f711a8b534227236ebe39880170eed
VanyaMark <markova.vanya@gmail.com>
2024-04-29 22:27:13 +0200

    Lifting state up in App in order to use activePlayer state in Player and GameBoard components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Player = ({initialName, symbol}) => {
+const Player = ({initialName, symbol, isActive }) => {
 
 const [playerName, setPlayerName] = useState(initialName)
 const [ isEditing, setIsEditing ] = useState(false)
@@ -24,7 +24,7 @@ if (isEditing) {
 }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined} >
     <span className="player">
         {editablePlayerName}
       <span className="player-symbol">{symbol}</span>
